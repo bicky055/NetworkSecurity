6999552631fa1c076033f4bdbe95c8675586db1b
Bicky jha <bickyjha963@gmail.com>
2026-03-03 11:58:44 +0530

    ETL Pipeline completed

---
+++ test_mongodb.py
@@ -0,0 +1,20 @@
+from pymongo.mongo_client import MongoClient
+import certifi
+
+ca = certifi.where()
+
+uri = "mongodb+srv://bickyjha055_db_user:Admin123@cluster0.7elfao1.mongodb.net/?retryWrites=true&w=majority"
+
+try:
+    client = MongoClient(
+        uri,
+        tls=True,
+        tlsCAFile=ca
+    )
+
+    client.admin.command('ping')
+    print("✅ Successfully connected to MongoDB Atlas!")
+
+except Exception as e:
+    print("❌ Connection Error:")
+    print(e)
